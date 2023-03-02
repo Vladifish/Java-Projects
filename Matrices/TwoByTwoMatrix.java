5149210b761f9ea6f7caafc1a175a61c84193220
Vladifish <101858972+Vladifish@users.noreply.github.com>
2023-03-02 18:59:12 +0800

    Started 2x2 Matrix

---
+++ Matrices/TwoByTwoMatrix.java
@@ -0,0 +1,9 @@
+package Matrices;
+
+public class TwoByTwoMatrix {
+    // I could make a matrix class altogether
+    double[][] product2x2Matrix(double[][] A, double[][] A) {
+        double[][] C = new double[2][2];
+        return C;
+    }
+}

86f0b4a3b3074502a05fb2f7f2a69e07d47de0bc
Vladifish <101858972+Vladifish@users.noreply.github.com>
2023-03-03 16:15:56 +0800

    started arithmetic mean

--- Exceptions/ExceptionMeanSeries.java
+++ Exceptions/ExceptionMeanSeries.java
@@ -3,3 +3,39 @@ package Exceptions;
 public class ExceptionMeanSeries {
 
 }
+
+class Series {
+    private int n;
+    private double a, d;
+    private double sum;
+
+    Series() {
+        sum = 0;
+
+    }
+
+    void arithmetic() {
+        System.out.print("S = ");
+        for (int i = 1; i < n; i++) {
+            double value = a + (i - 1) * d;
+            System.out.print(value + ", ");
+            sum += value;
+        }
+        double lastValue = a + n - 1 * d;
+        System.out.println((a + n - 1 * d));
+        System.out.println();
+    }
+
+    public void setN(int n) {
+        this.n = n;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public void setD(double d) {
+        this.d = d;
+    }
+
+}

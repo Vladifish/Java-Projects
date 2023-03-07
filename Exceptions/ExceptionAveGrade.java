7954de9d2f1588ed7424dc10f8d72ed9e91d3f0a
Vladifish <101858972+Vladifish@users.noreply.github.com>
2023-03-07 16:00:01 +0800

    created my own error

---
+++ Exceptions/ExceptionAveGrade.java
@@ -0,0 +1,12 @@
+// Vladimir Gray P. Velazco 1-CSC
+package Exceptions;
+
+class outOfRangeError extends Error {
+    outOfRangeError() {
+        System.out.println("");
+    }
+
+    outOfRangeError(String message) {
+        System.out.println(message);
+    }
+}

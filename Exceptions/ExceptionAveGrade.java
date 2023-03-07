3531f572a573e9e1f6bc4b6b7b6a15ebc61f1b76
Vladifish <101858972+Vladifish@users.noreply.github.com>
2023-03-07 16:08:15 +0800

    Finished MP

--- Exceptions/ExceptionAveGrade.java
+++ Exceptions/ExceptionAveGrade.java
@@ -1,12 +1,29 @@
 // Vladimir Gray P. Velazco 1-CSC
 package Exceptions;
 
+import java.util.Scanner;
+
 class outOfRangeError extends Error {
     outOfRangeError() {
-        System.out.println("");
+        System.out.println("Error: Values Not In Range!");
     }
 
     outOfRangeError(String message) {
         System.out.println(message);
+    }
+}
+
+public class ExceptionAveGrade {
+    public static void main(String[] args) throws outOfRangeError {
+        Scanner console = new Scanner(System.in);
+        double sum = 0;
+        System.out.println("Enter 5 Scores : Values Should be Between 0 and 100 (inclusive)");
+        for (int i = 0; i < 5; i++) {
+            double x = console.nextDouble();
+            if (x < 0 || x > 100)
+                throw new outOfRangeError("Error: Numbers Should Be Between 0-100");
+            sum += x;
+        }
+        System.out.println("Average = " + sum / 5);
     }
 }

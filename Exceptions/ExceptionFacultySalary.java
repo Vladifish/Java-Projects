e69c7c524620a784c5caed592aab904dd27c1bd4
Vladifish <101858972+Vladifish@users.noreply.github.com>
2023-03-07 16:19:37 +0800

    Added faculty classes and their respective values

--- Exceptions/ExceptionFacultySalary.java
+++ Exceptions/ExceptionFacultySalary.java
@@ -17,4 +17,72 @@ interface Faculty {
 
 class Instructor implements Faculty {
 
+    @Override
+    public int getLectureRate() {
+        return 2100;
+    }
+
+    @Override
+    public int getLabRate() {
+        return 2450;
+    }
+
+    @Override
+    public int getAllowablePreparations() {
+        return 3;
+    }
+
+    @Override
+    public double getPreparationRate() {
+        return 0.023;
+    }
+
+}
+
+class AsstProfessor implements Faculty {
+
+    @Override
+    public int getLectureRate() {
+        return 2550;
+    }
+
+    @Override
+    public int getLabRate() {
+        return 2950;
+    }
+
+    @Override
+    public int getAllowablePreparations() {
+        return 4;
+    }
+
+    @Override
+    public double getPreparationRate() {
+        return 0.03;
+    }
+
+}
+
+class Professor implements Faculty {
+
+    @Override
+    public int getLectureRate() {
+        return 3160;
+    }
+
+    @Override
+    public int getLabRate() {
+        return 3500;
+    }
+
+    @Override
+    public int getAllowablePreparations() {
+        return 4;
+    }
+
+    @Override
+    public double getPreparationRate() {
+        return 0.04;
+    }
+
 }

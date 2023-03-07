692d7238c4a9ff609ba0f5bec61ae0d7973f6ca9
Vladifish <101858972+Vladifish@users.noreply.github.com>
2023-03-07 16:15:53 +0800

    Created Faculty Interface

--- Exceptions/ExceptionFacultySalary.java
+++ Exceptions/ExceptionFacultySalary.java
@@ -1,5 +1,20 @@
+// Vladimir Gray P. Velazco 1-CSC
 package Exceptions;
 
 public class ExceptionFacultySalary {
+
+}
+
+interface Faculty {
+    int getLectureRate();
+
+    int getLabRate();
+
+    int getAllowablePreparations();
+
+    double getPreparationRate();
+}
+
+class Instructor implements Faculty {
 
 }

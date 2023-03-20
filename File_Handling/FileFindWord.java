36a60886f54f40b7168cf1172d42f479468c238b
Vladifish <101858972+Vladifish@users.noreply.github.com>
2023-03-20 09:20:01 +0800

    added a try catch stuff

--- File_Handling/FileFindWord.java
+++ File_Handling/FileFindWord.java
@@ -1,7 +1,22 @@
+// Vladimir Gray Velazco 1CSC
 package File_Handling;
 
+import java.io.File;
+import java.io.IOException;
+
 public class FileFindWord {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        File f0 = new File("File_Handling/Epic.txt");
+        try {
+            if (f0.createNewFile()) {
+                System.out.println("File " + f0.getName() + " Created Successfully"); // returns file name
+            } else {
+                System.out.println("File already in directory");
+            }
+        } catch (IOException e) {
+            System.out.println("An unexpected error has occured");
+            e.printStackTrace();
+        }
 
     }
 }

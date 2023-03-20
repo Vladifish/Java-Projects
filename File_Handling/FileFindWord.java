58feba5e5042ec0f6f5ece55ad3caa8a0a94478b
Vladifish <101858972+Vladifish@users.noreply.github.com>
2023-03-20 10:35:24 +0800

    Renamed some files, fixing what is really needed

--- File_Handling/FileFindWord.java
+++ File_Handling/FileFindWord.java
@@ -6,28 +6,6 @@ import java.util.Scanner;
 
 public class FileFindWord {
     public static void main(String[] args) throws IOException {
-        File f0 = new File("File_Handling/Editables/Epic.txt");
-        try {
-            if (f0.createNewFile()) {
-                System.out.println("File " + f0.getName() + " Created Successfully"); // returns file name
-            } else {
-                System.out.println("File already in directory");
-            }
-            System.out.print(f0.getName() + " ");
-            System.out.printf("Writable: %B :: Readable: %B\n", f0.canWrite(), f0.canRead());
-
-            Scanner console = new Scanner(System.in);
-            FileWriter fileWriter = new FileWriter(f0);
-
-            System.out.println("Input a word to be added to file: ");
-            fileWriter.write(console.next());
-            System.out.println("File Writing Successful");
-            fileWriter.close();
-
-        } catch (IOException e) {
-            System.out.println("An unexpected error has occured");
-            e.printStackTrace();
-        }
 
     }
 }

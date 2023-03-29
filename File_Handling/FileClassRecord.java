2b404008adeddde6435aecc880c9738929aa8405
Vladifish <101858972+Vladifish@users.noreply.github.com>
2023-03-29 10:36:36 +0800

    Added GUI Menu Message

--- File_Handling/FileClassRecord.java
+++ File_Handling/FileClassRecord.java
@@ -6,12 +6,24 @@ import javax.swing.*;
 // I will not write the entire thing in GUI, just trying out buttons rn
 public class FileClassRecord {
     public static void main(String[] args) {
-        do {
+        while (optionMessage() != 'C') {
 
-        } while (run());
+        }
+        // GOOD BYE!
     }
 
-    public static boolean run() {
-
+    public static char optionMessage() {
+        String openingMessage = "Menu\n" +
+                "A. Add Record\n" +
+                "B. Display Class Record\n" +
+                "C. Quit";
+        // magical incantation
+        // sends a message, capitalizes then gets the 1st letter of the input
+        char letterInput = JOptionPane.showInputDialog(openingMessage).toUpperCase().charAt(0);
+        while (letterInput != 'A' && letterInput != 'B' && letterInput != 'C') {
+            JOptionPane.showMessageDialog(null, "Error: Unrecognized Input");
+            letterInput = JOptionPane.showInputDialog(openingMessage).toUpperCase().charAt(0);
+        }
+        return letterInput;
     }
 }

c9b41f35fc90a5ef07b58996a76d82adec933656
Vladifish <101858972+Vladifish@users.noreply.github.com>
2023-03-29 11:25:29 +0800

    Extended JOptionPane

--- File_Handling/FileClassRecord.java
+++ File_Handling/FileClassRecord.java
@@ -4,39 +4,44 @@ package File_Handling; // maybe also some GUI manipulation
 import javax.swing.*;
 import java.io.*;
 
-public class FileClassRecord {
+public class FileClassRecord extends JOptionPane {
     public static void main(String[] args) {
         char choice;
         final int COLUMNS = 5;
         final int ROWS = 7;
         while ((choice = optionMessage()) != 'C') {
-            final String className = JOptionPane.showInputDialog(null, "Enter Class Name");
+            final String className = showInputDialog(null, "Enter Class Name");
             String path = "File_Handling/" + className + ".csv"; // edit your path, mine is like this so it might not
                                                                  // work for everyone
             File classRecord = new File(path);
 
             try {
                 if (classRecord.createNewFile()) {
-                    System.out.println("File " + classRecord.getName() + " Created Successfully"); // returns file
-                                                                                                   // name
+                    showMessageDialog(null, "File " + classRecord.getName() + " Created Successfully"); // returns file
+                    // name
                 } else {
-                    System.out.println("File already in directory");
+                    showMessageDialog(null, "File already in directory");
                 }
                 if (choice == 'A') {
                     FileWriter csvEditor = new FileWriter(classRecord);
+
                     String heading[] = { "Student", "Quiz 1", "Quiz 2", "Quiz 3", "Average" };
                     for (String word : heading)
                         csvEditor.append(word + ",");
+                    csvEditor.append("\n");
 
-                    for (int i = 0; i < 3; i++)
-                        csvEditor.close();
+                    final int NUM_STUDENTS = 3;
+                    for (int i = 0; i < NUM_STUDENTS; i++) {
+                        csvEditor.append(i + ",");
+                    }
+                    csvEditor.close();
                 }
             } catch (IOException e) {
-                System.out.println("An unexpected error has occured");
+                showMessageDialog(null, "An unexpected error has occured");
                 e.printStackTrace();
             }
         }
-        JOptionPane.showMessageDialog(null, "Exiting :: GOOD BYE!", "EXITING", JOptionPane.INFORMATION_MESSAGE);
+        showMessageDialog(null, "Exiting :: GOOD BYE!", "EXITING", JOptionPane.INFORMATION_MESSAGE);
 
         // GOOD BYE!
     }
@@ -48,11 +53,15 @@ public class FileClassRecord {
                 "C. Quit";
         // magical incantation
         // sends a message, capitalizes then gets the 1st letter of the input
-        char letterInput = JOptionPane.showInputDialog(openingMessage).toUpperCase().charAt(0);
+        char letterInput = showInputDialog(openingMessage).toUpperCase().charAt(0);
         while (letterInput != 'A' && letterInput != 'B' && letterInput != 'C') {
-            JOptionPane.showMessageDialog(null, "Unrecognized Input", "Input Error", JOptionPane.ERROR_MESSAGE);
-            letterInput = JOptionPane.showInputDialog(openingMessage).toUpperCase().charAt(0);
+            showMessageDialog(null, "Unrecognized Input", "Input Error", JOptionPane.ERROR_MESSAGE);
+            letterInput = showInputDialog(openingMessage).toUpperCase().charAt(0);
         }
         return letterInput;
+    }
+
+    public static void validateQuizScore() {
+
     }
 }

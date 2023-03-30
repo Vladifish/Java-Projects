4ae1bd9e491a8310204bb1f79c1d1fd9175991d0
Vladifish <101858972+Vladifish@users.noreply.github.com>
2023-03-30 17:18:19 +0800

    Started FileJennySalary

--- File_Handling/FileJennySalary.java
+++ File_Handling/FileJennySalary.java
@@ -1,4 +1,24 @@
+// Vladimir Gray P. Velazco
 package File_Handling;
+
+// Conditions:
+// Base Rate $10
+// Overtime $1.50 >8 hrs
+// Bonus: +12% on Saturday, +50% on Sunday
+// Saturday and Sunday are not counted in the 40hr week
+
+// Given:
+// hrs. worked each day in a week Format (Sunday, ..., Saturday)
+// Compute:
+// Salary for the week
+// Input:
+// File, positive integers <= 24
+// Output:
+// dollars rounded up to the nearest penny
+// sample: $2.41666 = $2.42
+// 5 sets of data
+
+import java.io.*;
 
 public class FileJennySalary {
 

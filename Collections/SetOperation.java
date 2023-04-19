7b5c3d934d9d82a699178851eb580625ce4ef97a
Vladifish <101858972+Vladifish@users.noreply.github.com>
2023-04-19 13:43:52 +0800

    Changed File Name of SetOperations

---
+++ Collections/SetOperation.java
@@ -0,0 +1,30 @@
+package Collections;
+
+import java.util.*;
+
+public class SetOperation {
+    public static void main(String[] args) {
+
+    }
+}
+
+// Menu
+// 1. Add element to Set A
+// 2. Add element to Set B
+// 3. Remove element from Set A
+// 4. Remove element from Set B
+// 5. Display Set A
+// 6. Display Set B
+// 7. Display Union of A and B
+// 8. Display Intersection of A and B
+// 9. Display A – B
+// 10. Display B – A
+// 11. Check if A is a subset of B
+// 12. Check if B is a subset of A
+// 13. Quit
+
+class Operations<T> {
+    // static String union(HashSet<T> a, HashSet<T> b) {
+
+    // }
+}

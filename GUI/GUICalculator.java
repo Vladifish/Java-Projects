5e9d9cfd424b483e8a8ac878a633b8e6f3608eb6
Vladifish <101858972+Vladifish@users.noreply.github.com>
2023-05-10 09:08:28 +0800

    FINISHED: GUI calculator, TODO: button resizing

--- GUI/GUICalculator.java
+++ GUI/GUICalculator.java
@@ -13,20 +13,25 @@ public class GUICalculator {
 class OutputFrame extends JFrame {
     private Label l1, l2, l3;
     private TextField tf1, tf2, tf3;
-    private Panel p1, p2, p3;
+    private Panel p1, p2, p3, p4;
+    private Button add, subt, mult, divide, canc;
 
     OutputFrame() {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        l1 = new Label("Label 1");
-        tf1 = new TextField("Textfield 1");
-        l2 = new Label("Label 2");
-        tf2 = new TextField("Textfield 3");
-        l3 = new Label("Label 3");
-        tf3 = new TextField("Textfield 3");
+        setTitle("GUI Calculator");
+        setLayout(new GridLayout(4, 1, 10, 0));
 
         p1 = new Panel(new FlowLayout());
         p2 = new Panel(new FlowLayout());
         p3 = new Panel(new FlowLayout());
+        p4 = new Panel(new FlowLayout());
+
+        l1 = new Label("First Number");
+        tf1 = new TextField("0.0");
+        l2 = new Label("Second Number");
+        tf2 = new TextField("0.0");
+        l3 = new Label("Answer");
+        tf3 = new TextField("0.0");
 
         p1.add(l1);
         p1.add(tf1);
@@ -35,12 +40,26 @@ class OutputFrame extends JFrame {
         p3.add(l3);
         p3.add(tf3);
 
+        add = new Button("+");
+        subt = new Button("-");
+        mult = new Button("*");
+        divide = new Button("/");
+        canc = new Button("C");
+
+        p4.add(add);
+        p4.add(subt);
+        p4.add(mult);
+        p4.add(divide);
+        p4.add(canc);
+
         add(p1);
         add(p2);
         add(p3);
-
-        setBounds(500, 500, 300, 300);
+        add(p4);
         pack();
+
+        setLocationRelativeTo(null);
+        setSize(400, 600);
         setVisible(true);
     }
 }

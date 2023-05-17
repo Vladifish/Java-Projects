bbdabb7f96b2aacc56c8984981d8eca975fdebb2
Vladifish <101858972+Vladifish@users.noreply.github.com>
2023-05-17 11:09:25 +0800

    COMPLETED: header areas

--- GUI/GUIMeal.java
+++ GUI/GUIMeal.java
@@ -18,9 +18,36 @@ class MenuFrame extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setTitle("Ordertaker");
         setLayout(new GridLayout(3, 1));
+        setSize(600, 600);
+        setLocationRelativeTo(null);
 
-        header = new Panel(new GridLayout(2, 1));
+        createHeader();
+        add(header);
+    }
 
+    void createHeader() {
+        header = new Panel(new GridLayout(1, 2));
+
+        // Creating the header
+        Component[] headerObjects = new Component[2];
+
+        Label mealCode = new Label("Meal Code");
+        mealCode.setAlignment(Label.CENTER);
+        headerObjects[0] = mealCode;
+
+        Panel rightPanel = new Panel(new GridLayout(2, 1));
+        Label size = new Label("Size");
+        size.setAlignment(Label.CENTER);
+        rightPanel.add(size);
+
+        Panel sizes = new Panel(new GridLayout(1, 2));
+        sizes.add(new Label("Regular - R"));
+        sizes.add(new Label("Upsize - U"));
+        rightPanel.add(sizes);
+
+        headerObjects[1] = rightPanel;
+
+        addComponentsToPanel(header, headerObjects);
     }
 
     void addComponentsToPanel(Panel panel, Component[] components) {

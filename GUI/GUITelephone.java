91b562bbcf436d8a0a2eb5af32a268964396eff6
Vladifish <101858972+Vladifish@users.noreply.github.com>
2023-05-18 15:34:21 +0800

    STARTED: TELEPHONE.java

--- GUI/GUITelephone.java
+++ GUI/GUITelephone.java
@@ -1,7 +1,16 @@
 // Vladimir Gray P. Velazco 1CSC
 package GUI;
 
+import javax.swing.*;
+import javax.swing.event.*;
+
 public class GUITelephone {
+
+    final static int CALL_RATE = 4;
+    final static double NIGHT_DISCOUNT = 0.5;
+    final static double EXTEND_DISCOUNT = 0.15;
+    final static double EVAT = 0.12;
+
     public static void main(String[] args) {
 
     }

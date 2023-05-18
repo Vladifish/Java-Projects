8040271c9ca6e0e54e03cd4c8c9d41a4e6bd8404
Vladifish <101858972+Vladifish@users.noreply.github.com>
2023-05-18 16:33:23 +0800

    ADDED: content pane, FIXED: input columns

--- GUI/GUITelephone.java
+++ GUI/GUITelephone.java
@@ -23,20 +23,32 @@ public class GUITelephone {
 class TelephoneUI extends JFrame {
 
     private ButtonGroup dayOrNight;
-    JRadioButton dayButton, nightButton;
+    private JRadioButton dayButton, nightButton;
     private JPanel CostOutputPanel, InputPanel;
     private JTextField callHoursInput;
 
     TelephoneUI() {
-        setSize(300, 500);
+        setSize(300, 200);
         setLocationRelativeTo(null);
+        setLayout(new GridLayout(1, 1));
+
+        JPanel contentPanel = new JPanel();
+        contentPanel.setLayout(new GridLayout(2, 1, 15, 0));
+        contentPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        setContentPane(contentPanel);
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setTitle("GUI Calculator");
-        setLayout(new GridLayout(2, 1, 15, 0));
 
+        // setupOutputPanel();
+        // add(CostOutputPanel);
         setupInputPanel();
         add(InputPanel);
+    }
+
+    private void setupOutputPanel() {
+
     }
 
     private void setupButtons() {
@@ -58,12 +70,24 @@ class TelephoneUI extends JFrame {
         buttons.add(nightButton);
 
         JPanel textInput = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        textInput.add(new JLabel("Call Length: "));
-        callHoursInput = new JTextField("0");
-        callHoursInput.setSize(50, 20);
+        textInput.add(new JLabel("Call Length (minutes): "));
+        callHoursInput = new JTextField("0", 3);
+        // callHoursInput.setSize(50, 20);
         textInput.add(callHoursInput);
 
         InputPanel.add(buttons);
         InputPanel.add(textInput);
+    }
+
+    public char getTime() {
+        if (dayButton.isSelected()) {
+            return 'D';
+        } else {
+            return 'N';
+        }
+    }
+
+    public int getHours() {
+        return Integer.parseInt(callHoursInput.getText());
     }
 }

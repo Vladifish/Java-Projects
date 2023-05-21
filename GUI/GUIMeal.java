0f362db6b756ec223f38b9f503d5952ae31bb566
Vladifish <101858972+Vladifish@users.noreply.github.com>
2023-05-21 23:35:45 +0800

    Last Added Item Now Updates, TODO: fix disappearing text bug

--- GUI/GUIMeal.java
+++ GUI/GUIMeal.java
@@ -3,10 +3,13 @@
 package GUI;
 
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
 import java.util.Stack;
 
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.border.EmptyBorder;
 
 public class GUIMeal {
@@ -18,13 +21,14 @@ public class GUIMeal {
 
 class MenuFrame extends JFrame {
     private Panel header, body, orders, payment; // Main Panels
-    private Panel lastItemAdded; // Utility Panels
-    private Button submitButton, undoButton;
+    private Panel lastItemAdded, orderHeader, lastAddedOrder; // Utility Panels
+    private Button addButton, undoButton;
     private Label netCost, grossCost;
 
     TextField amountField;
-    JComboBox<String> mealOptions;
+    JComboBox<String> mealOptions, sizeOptions;
     Stack<String[]> order_stack = new Stack<>();
+    HashMap<String, String[]> MENU_MAP = new HashMap<>();
 
     final static String[] MEAL_OPTIONS = {
             "Meal A", "Meal B", "MEAL C",
@@ -36,11 +40,15 @@ class MenuFrame extends JFrame {
             "B", "68.75", "85.70",
             "C", "70.25", "95.60",
             "-----", "-----", "-----",
-            "Sides", "", "",
+            "Sides", "(No sizes)", "",
             "Extra Rice", "20.00", "",
             "Salad", "60.00", "",
             "Soup", "55.00", "",
             "Dessert", "85.00", ""
+    };
+
+    final static String[] SIZES = {
+            "R", "U"
     };
 
     MenuFrame() {
@@ -49,6 +57,20 @@ class MenuFrame extends JFrame {
         setLayout(new GridLayout(4, 1));
         setSize(600, 600);
         setLocationRelativeTo(null);
+
+        for (int i = 0; i < MENU.length / 3; i++) {
+            if (i == 3 && i == 4)
+                continue;
+
+            String[] sizedPrices = new String[2];
+            sizedPrices[0] = MENU[i * 3 + 1]; // either the regular price or the default
+            if (i > 4) {
+                sizedPrices[1] = MENU[i * 3 + 1];
+            } else {
+                sizedPrices[1] = MENU[i * 3 + 2];
+            }
+            MENU_MAP.put(MENU[i * 3], sizedPrices);
+        }
 
         createHeader();
         add(header);
@@ -63,11 +85,13 @@ class MenuFrame extends JFrame {
     private void createPaymentPanel() {
         payment = new Panel(new GridLayout(2, 2));
         lastItemAdded = new Panel(new GridLayout(2, 1));
+        orderHeader = createRow("Meal", "Size", "Amount");
+        lastAddedOrder = createRow("---", "---", "---");
+        lastItemAdded.add(orderHeader);
+        lastItemAdded.add(lastAddedOrder);
         displayLastAdded(); // the last item added, will be completely empty at first run
         payment.add(lastItemAdded);
         payment.add(new Panel()); // Filler
-
-        // Lower Half of Payment Panel
 
         // The display for the net and gross cost
         Panel costs = new Panel(new GridLayout(2, 2));
@@ -82,19 +106,38 @@ class MenuFrame extends JFrame {
 
         // Confirms orders
         Panel finalChanges = new Panel(new FlowLayout(FlowLayout.CENTER, 20, 0));
-        submitButton = new Button("SUBMIT");
+        addButton = new Button("ADD");
         undoButton = new Button("UNDO");
 
-        finalChanges.add(submitButton);
+        addButton.addActionListener(addToOrder);
+
+        finalChanges.add(addButton);
         finalChanges.add(undoButton);
         payment.add(finalChanges);
     }
+
+    private ActionListener addToOrder = (e) -> {
+        int amt = getAmount();
+        String size = getMealSize();
+        String type = getMealType();
+        if (amt != -1 && !size.equals("!") && !type.equals("!")) {
+            // those are mostly dead values
+            String outputStrings[] = new String[3];
+            outputStrings[0] = type;
+            outputStrings[1] = size;
+            outputStrings[2] = amt + "";
+
+            order_stack.push(outputStrings);
+            displayLastAdded();
+        }
+    };
 
     private void createOrderFields() {
         orders = new Panel(new FlowLayout(FlowLayout.RIGHT, 0, 0));
 
         // Dropdown List for Meal Type
         orders.add(new Label("Meal Type:"));
+
         mealOptions = new JComboBox<>(MEAL_OPTIONS);
         mealOptions.setBorder(new EmptyBorder(0, 0, 0, /* right: */10));
         orders.add(mealOptions);
@@ -103,6 +146,14 @@ class MenuFrame extends JFrame {
         orders.add(new Label("Amount:"));
         amountField = new TextField(2);
         orders.add(amountField);
+        Panel fillerSpace = new Panel();
+        fillerSpace.setSize(5, 0);
+        orders.add(fillerSpace);
+
+        // Fields for Meal Size, size shouldn't matter for sides
+        orders.add(new Label("Size (Unnaplicable for Sides):"));
+        sizeOptions = new JComboBox<>(SIZES);
+        orders.add(sizeOptions);
     }
 
     private void createBody() {
@@ -154,20 +205,67 @@ class MenuFrame extends JFrame {
         return row;
     }
 
+    private Panel createRow(String c1, String c2, String c3, String c4) {
+        Panel row = new Panel(new GridLayout(1, 4));
+        row.add(new Label(c1, Label.CENTER));
+        row.add(new Label(c2, Label.CENTER));
+        row.add(new Label(c3, Label.CENTER));
+        row.add(new Label(c4, Label.CENTER));
+        return row;
+    }
+
     /**
      * Reads the order stack and adds it to the display
      * If the stack is empty, then the output should also be empty
      */
     private void displayLastAdded() {
-        Panel orderHeader = createRow("Meal", "Size", "Amount");
-        Panel lastAddedOrder;
+        lastItemAdded.remove(lastAddedOrder); // resets the board
+
         if (!order_stack.isEmpty()) {
             String[] lastOrder = order_stack.peek();
             lastAddedOrder = createRow(lastOrder[0], lastOrder[1], lastOrder[2]);
         } else {
             lastAddedOrder = createRow("-", "-", "-"); // Completely Empty
         }
-        lastItemAdded.add(orderHeader);
         lastItemAdded.add(lastAddedOrder);
     }
+
+    public int getAmount() {
+        int amount;
+        try {
+            amount = Integer.parseInt(amountField.getText());
+            if (amount < 0) {
+                JOptionPane.showMessageDialog(null, "Amount must be positive or 0", "ERROR",
+                        JOptionPane.WARNING_MESSAGE);
+            }
+        } catch (NullPointerException e) {
+            amount = -1;
+            JOptionPane.showMessageDialog(null, "Amount Field is Blank", "ERROR", JOptionPane.WARNING_MESSAGE);
+        } catch (NumberFormatException e) {
+            amount = -1;
+            JOptionPane.showMessageDialog(null, "Amount must be an integer", "ERROR", JOptionPane.WARNING_MESSAGE);
+        }
+        return amount;
+    }
+
+    public String getMealType() {
+        String mealType;
+        try {
+            mealType = (String) mealOptions.getSelectedItem();
+        } catch (NullPointerException e) {
+            mealType = "!";
+        }
+        return mealType;
+    }
+
+    public String getMealSize() {
+        String mealSize;
+        try {
+            mealSize = (String) sizeOptions.getSelectedItem();
+        } catch (NullPointerException e) {
+            mealSize = "!";
+        }
+        return mealSize;
+    }
+
 }
